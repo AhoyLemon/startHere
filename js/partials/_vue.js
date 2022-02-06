483dafdcc7b5de5f7951434339ce313db3ab1d7c
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2022-02-06 16:55:57 -0600

    Mainly concerned with headlines.

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -1,7 +1,9 @@
 var app = new Vue({
   el: '#app',
   data: {
-    h1: 'It works.'
+    h1: "It works.",
+    h2: "This is the startHere template.",
+    h3: "Whenever you're ready, delete this text and start writing code."
   },
 
   methods: {
