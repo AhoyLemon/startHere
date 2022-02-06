2f5832ad9d5a74f053d476acbdf0fb8a0e268eb6
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2022-02-06 17:30:32 -0600

    Even better typography

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -2,8 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     h1: "It works.",
-    h2: "This is the startHere template.",
-    h3: "Whenever you're ready, delete this text and start writing code."
+    h5: "Whenever you're ready, delete this text and start writing code."
   },
 
   methods: {
